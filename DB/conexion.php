5a5cf0dbb5d8d9001116333dd0722206c251cb51
IsylolXD <133933649+IsylolXD@users.noreply.github.com>
2024-08-28 09:26:10 -0600

    Cambios Finales

--- DB/conexion.php
+++ DB/conexion.php
@@ -2,9 +2,10 @@
 
 function Conecta()
 {
-    $base = new PDO("mysql:host=localhost:3306; dbname=db_restaurantes", "root", "soraraper1");
+    $base = new PDO("mysql:host=localhost:3309; dbname=miercoles_bd", "root", ""); //OJO CAMBIAR PUERTO EN "mysql:host=localhost:3309;" DEBIDO A QUE EL PUERTO UTILIZADO POR MI PERSONA ES DIFERENTE
     $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $base->exec("SET CHARACTER SET utf8");
+    $base->exec("USE db_restaurantes");  
 
     return $base;
 }
